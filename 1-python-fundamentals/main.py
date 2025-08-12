a869e1c5fad9da91491585ba098d4b884f9f4fa4
zhudiana <yoditfentaw90@gmail.com>
2025-08-13 01:52:10 +0200

    type-hints added

--- 1-python-fundamentals/main.py
+++ 1-python-fundamentals/main.py
@@ -1,14 +1,4 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+def say_hi(name: str) -> str:
+    return f"Hi {name}"
 
-    def print_info(self):
-        print(f"This is {self.name} and I am {self.age} years old.")
-
-# Child class
-class Student(Person):
-    pass
-
-x = Student("John", 34)
-x.print_info()
+print(say_hi(6))
